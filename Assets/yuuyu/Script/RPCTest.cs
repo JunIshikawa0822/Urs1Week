a8eb45140df45b1fe8e56db95f895b3e31185593
UyYuUyYu <104560044+UyYuUyYu@users.noreply.github.com>
2024-06-24 21:42:29 +0900

    AddRPC

---
+++ Assets/yuuyu/Script/RPCTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RPCTest : MonoBehaviourPunCallbacks
+{
+    [ContextMenu("RPC")]
+    public void RPCt()
+    {
+        photonView.RPC(nameof(RpcSendMessage), RpcTarget.All, "こんにちは");
+    }
+
+
+    [PunRPC]
+    private void RpcSendMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
