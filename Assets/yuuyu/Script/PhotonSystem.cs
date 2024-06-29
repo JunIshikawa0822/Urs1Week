8bdb1926960896b2ba57cf2e338f95c11814353d
UyYuUyYu <104560044+UyYuUyYu@users.noreply.github.com>
2024-06-30 01:55:52 +0900

    MoelChange

--- Assets/yuuyu/Script/PhotonSystem.cs
+++ Assets/yuuyu/Script/PhotonSystem.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using ExitGames.Client.Photon;
 
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
-public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallbacks,IInRoomCallbacks
+public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallbacks
 {
-
+    //,IInRoomCallbacks
     public override void SetUp()
     {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
@@ -79,14 +79,15 @@ public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallba
         */
 
     }
+    /*
     public void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
     {
         Debug.Log("MatchOk");
         gameStat.isMatchOk = true;
         throw new System.NotImplementedException();
     }
-
-
+    */
+    
     // Photonのサーバーから切断された時に呼ばれるコールバック
     void IConnectionCallbacks.OnDisconnected(DisconnectCause cause)
     {
@@ -94,6 +95,7 @@ public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallba
         // PUNのコールバック対象の登録を解除する
         PhotonNetwork.RemoveCallbackTarget(this);
     }
+    
     
     void IConnectionCallbacks.OnConnected() { }
     //void IConnectionCallbacks.OnDisconnected(DisconnectCause cause) { }
@@ -117,7 +119,7 @@ public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallba
     
     
     
-    
+    /*
     public void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
     {
         throw new System.NotImplementedException();
@@ -137,5 +139,6 @@ public class PhotonSystem : SystemBase, IConnectionCallbacks, IMatchmakingCallba
     {
         throw new System.NotImplementedException();
     }
+    */
     
 }
